ca544d3681a7536411bed61b49eef7efcbb8680d
SirSertile <rezdevhead@gmail.com>
2018-12-10 13:39:42 -0500

    automatically pushed at Mon Dec 10 13:39:42 EST 2018

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ cd $HOME/DotfileBackup
 #copy the current dotfiles to the current address, then delete them and symlink them? 
 rm -r DotfileBackup/dotfiles
 cp $HOME/.xinitrc ./DotfileBackup/dotfiles
-cp $HOME/.config/herbstluftwm/autostart DotfileBackup/dotfiles
+cp $HOME/.config/herbstluftwm/autostart ./dotfiles
 cp $HOME/.config/lemonbar.sh DotfileBackup/dotfiles
 cp $HOME/.config/scriptswitcher.sh DotfileBackup/dotfiles
 cp $HOME/XTerm DotfileBackup/dotfiles
