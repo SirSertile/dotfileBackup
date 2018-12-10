dd095bcaeb2e7695c3dd982e8aeddd1a8005b305
SirSertile <rezdevhead@gmail.com>
2018-12-10 13:23:03 -0500

    automatically pushed at Mon Dec 10 13:23:03 EST 2018

--- backup.sh
+++ backup.sh
@@ -6,12 +6,12 @@ herbstclient use BACKUP
 cd $HOME/DotfileBackup
 #linking of files
 #copy the current dotfiles to the current address, then delete them and symlink them? 
-cp $HOME/.xinitrc dotfiles/.xinitrc
-cp $HOME/.config/herbstluftwm/autostart dotfiles/autostart
-cp $HOME/.config/lemonbar.sh dotfiles/lemonbar.sh
-cp $HOME/.config/scriptswitcher.sh dotfiles/scriptswitcher.sh
-cp $HOME/XTerm dotfiles/XTerm
-cp $HOME/.Xresources dotfiles/.Xresources
+cp $HOME/.xinitrc ./dotfiles/.xinitrc
+cp $HOME/.config/herbstluftwm/autostart ./dotfiles/autostart
+cp $HOME/.config/lemonbar.sh ./dotfiles/lemonbar.sh
+cp $HOME/.config/scriptswitcher.sh ./dotfiles/scriptswitcher.sh
+cp $HOME/XTerm ./dotfiles/XTerm
+cp $HOME/.Xresources ./dotfiles/.Xresources
 rm -r dotfiles
 mkdir dotfiles
 
@@ -28,9 +28,9 @@ herbstclient use $inittag
 herbstclient merge_tag BACKUP $inittag
 
 #make symlinks for them to keep around for editability 
-ln -s $HOME/.xinitrc dotfiles/.xinitrc
-ln -s $HOME/.config/herbstluftwm/autostart dotfiles/autostart
-ln -s $HOME/.config/lemonbar.sh dotfiles/lemonbar.sh
-ln -s $HOME/.config/scriptswitcher.sh dotfiles/scriptswitcher.sh
-ln -s $HOME/XTerm dotfiles/XTerm
-ln -s $HOME/.Xresources dotfiles/.Xresources
+ln -s $HOME/.xinitrc ./dotfiles/.xinitrc
+ln -s $HOME/.config/herbstluftwm/autostart ./dotfiles/autostart
+ln -s $HOME/.config/lemonbar.sh ./dotfiles/lemonbar.sh
+ln -s $HOME/.config/scriptswitcher.sh ./dotfiles/scriptswitcher.sh
+ln -s $HOME/XTerm ./dotfiles/XTerm
+ln -s $HOME/.Xresources ./dotfiles/.Xresources
