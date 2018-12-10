c0f82c4b3cc9e3aabeea0425fe3388d72930ba5c
SirSertile <rezdevhead@gmail.com>
2018-12-10 13:07:40 -0500

    automatically pushed at Mon Dec 10 13:07:40 EST 2018

--- backup.sh
+++ backup.sh
@@ -1,3 +1,14 @@
 #!/bash/sh
-#copy files to the directory 
-
+#copy files to the directory
+inittag="$(herbstclient attr tags.focus.name)"
+herbstclient add BACKUP
+herbstclient use BACKUP
+cd $HOME/DotfileBackup
+xterm -e "cd $HOME/Documents/DotfileBackup;\
+git init; \
+git add ./dotfiles; \
+git commit -m 'automatically pushed at $(date)';\
+git push -u origin master; read"
+herbstclient close_or_remove
+herbstclient use $inittag
+herbstclient merge_tag BACKUP $inittag
