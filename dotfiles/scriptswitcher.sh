34b39b3a55fb4cddd102c802c34c4f851f0e9bf2
SirSertile <rezdevhead@gmail.com>
2018-12-15 20:22:35 -0500

    automatically pushed at Sat Dec 15 20:22:35 EST 2018

--- dotfiles/scriptswitcher.sh
+++ dotfiles/scriptswitcher.sh
@@ -1,5 +1,6 @@
 #curl wttr.in; read
-list=("Weather" "Calendar(all)" "Calendar(school)" "Calendar(personal)" "Calendar(work)")
+list=("Weather" "Calendar" "Calendar(all)" "Calendar(school)" "Calendar(personal)" "Calendar(work)")
+calendar=("All" "School" "Personal" "Work")
 function disprofi {
 	args=($(echo "$@" | tr ' ' '\n'))
 	main=''
@@ -10,33 +11,43 @@ function disprofi {
 	input="$(echo -e $main | rofi -dmenu -lines 5)"
 	echo $input
 }
-input=$(disprofi ${list[*]})
+
+case $(disprofi ${list[*]}) in
+"${list[0]}")
+	command="curl wttr.in/?m"
+	;;
+"${list[1]}")
+	case $(disprofi ${calendar[*]}) in
+		#ALL
+		${calendar[0]})
+			command="gcalcli calw 1 --configFolder /home/bluesteelblade/.config/gcalcli/ --calendar 'Class of 2022 Events' --calendar 'LCDI shifts' --calendar 'rezdevhead@gmail.com' --calendar 'david.serate@mymail.champlain.edu' --calendar 'Champlain Fall 2018 Semester'"
+		;;
+		#WORK
+		${calendar[1]})
+			command="gcalcli calw 1 --configFolder /home/bluesteelblade/.config/gcalcli/ --calendar 'LCDI shifts'"
+		;;
+		#SCHOOL
+		${calendar[2]})
+			command="gcalcli calw 1 --configFolder /home/bluesteelblade/.config/gcalcli/ --calendar 'Class of 2022 Events' --calendar 'LCDI shifts' --calendar 'david.serate@mymail.champlain.edu' --calendar 'Champlain Fall 2018 Semester'"
+		;;
+		#PERSONAL
+		${calendar[3]})
+			command="gcalcli calw 1 --configFolder /home/bluesteelblade/.config/gcalcli/ --calendar 'rezdevhead@gmail.com'"
+		;;
+	esac
+	;;
+*)
+	command=""
+	;;
+esac
+
 inittag="$(herbstclient attr tags.focus.name)"
 active="#002cff"
 transparent="#212121"
 normal="3a3a3a"
 herbstclient add EXTRA
 herbstclient use EXTRA
-case $input in
-"Weather")
-	command="curl wttr.in/?m"
-	;;
-"Calendar(all)")
-	command="gcalcli calw 1 --configFolder /home/bluesteelblade/.config/gcalcli/ --calendar 'Class of 2022 Events' --calendar 'LCDI shifts' --calendar 'rezdevhead@gmail.com' --calendar 'david.serate@mymail.champlain.edu' --calendar 'Champlain Fall 2018 Semester'"
-	;;
-"Calendar(work)")
-	command="gcalcli calw 1 --configFolder /home/bluesteelblade/.config/gcalcli/ --calendar 'LCDI shifts'"
-	;;
-"Calendar(school)")
-	command="gcalcli calw 1 --configFolder /home/bluesteelblade/.config/gcalcli/ --calendar 'Class of 2022 Events' --calendar 'LCDI shifts' --calendar 'david.serate@mymail.champlain.edu' --calendar 'Champlain Fall 2018 Semester'"
-	;;
-"Calendar(personal)")
-	command="gcalcli calw 1 --configFolder /home/bluesteelblade/.config/gcalcli/ --calendar 'rezdevhead@gmail.com'"
-	;;
-*)
-	command="echo $input not workin"
-	;;
-esac
+
 xterm -e "$command ; read"
 
 herbstclient close_or_remove
